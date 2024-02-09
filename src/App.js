2bb19d5d9358d596c30552ee121a18a0a7fadaa0
curso24 <tripleene333@gmail.com>
2024-02-09 13:26:15 +0100

    proyecto REAC sin logica ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
 import './App.css';
+import Player from './Player/Player'
 
 function App() {
-  return <div className="App">hola mundo</div>
-    
+  return (
+    <main>
+    <Player name="Player1" score={43} current={0} isActive={true}/>
+    <Player name="Player2" score={24} current={0} isActive={false}/>
+  
+  
+    <img src="dice-5.png" alt="Playing dice" className="dice" />
+    <button className="btn btn--new">:flechas_en_sentido_antihorario: New game</button>
+    <button className="btn btn--roll">:dado: Roll dice</button>
+    <button className="btn btn--hold">:bandeja_de_entrada: Hold</button>
+  </main>
+
+
+
+
+  )
       
    
   
